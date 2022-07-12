3ed06a5326e922a7e5e1cd7dee6dc7457585f53e
huynhduvan <72264118+huynhduvan@users.noreply.github.com>
2022-07-12 18:54:13 +0700

    Initial commit

---
+++ Lading-Pages/Lading-Pages/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Lading_Pages.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ Lading-PagesVer1/Lading-PagesVer1/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Lading_PagesVer1.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
